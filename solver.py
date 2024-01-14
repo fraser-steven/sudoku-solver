5312f7cebe0a428ea6d506c3917f1be2f06d5eed
fraser-steven <fraser.squash@gmail.com>
2024-01-14 14:06:55 +0000

    change flow of function to avoid assingment issues

--- solver.py
+++ solver.py
@@ -10,11 +10,44 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def solve(bo):
+    
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+    
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+            bo[row][col] = 0
+    return False
+
+def valid(bo, num, pos):
+    
+    #Check the row of board
+    for i in range(len(bo)):
+        if bo[pos[0][i]] == num and pos[1] != i:
+            return False
+    
+    box_a = pos[1] // 3
+    box_b = pos[0] // 3
+
+    for i in range(box_b*3, box_a*3+3):
+        for j in range(box_a*3, box_b*3+3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
 def print_board(bo):
 
     for i in range(len(bo)):
         if i % 3 == 0 and j != 0:
-            print("----------------")
+            print("- - - - - - - - - - - - -")
         
         for j in range(len(bo[0])):
             if i % 3 == 0 and j != 0:
@@ -33,36 +66,7 @@ def find_empty(bo):
     return None
 
 
-def valid(bo, num, pos):
-    
-    #Check the row of board
-    for i in range(len(bo)):
-        if bo[pos[0][i]] == num and pos[1] != i:
-            return False
-    
-    box_a = pos[1] // 3
-    box_b = pos[0] // 3
-
-    for i in range(box_b*3, box_a*3+3):
-        for j in range(box_a*3, box_b*3+3):
-            if bo[i][j] == num and (i,j) != pos:
-                return False
-    return True
-
-
-def solve(bo):
-    
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
-    
-    for i in range(1,10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
-
-            if solve(bo):
-                return True
-            bo[row][col] = 0
-    return False
+print_board(board)
+solve(board)
+print("=======================")
+print_board(board)
